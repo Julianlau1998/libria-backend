7f6c6db320038a548994902dae4162ed86c9b018
Julian Lau <julian.lau@aol.com>
2021-08-08 14:43:23 +0200

    env ver

--- utility/sql_helper.go
+++ utility/sql_helper.go
@@ -13,7 +13,7 @@ func NewDbClient() *sql.DB {
 	godotenv.Load(".env")
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		os.Getenv("HOST"), os.Getenv("PORT"), os.Getenv("dbUser"), os.Getenv("dbPassword"), os.Getenv("dbName"))
+		os.Getenv("HOST"), os.Getenv("DB_PORT"), os.Getenv("dbUser"), os.Getenv("dbPassword"), os.Getenv("dbName"))
 	conn, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(fmt.Sprintf("Verbindung mit Datenbank kann nicht hergestellt werden: %v", err))
